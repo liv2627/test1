3dc727c6a220e0718ba68d7a2be7b05042513c40
Oliwia Filipiak <152597789+liv2627@users.noreply.github.com>
2023-12-05 11:37:06 +0100

    Add files via upload

---
+++ zeto/src/main/java/com/practise/zeto/buyers/BuyerEntity.java
@@ -0,0 +1,29 @@
+package com.practise.zeto.buyers;
+
+import com.practise.zeto.global.GlobalEntity;
+import com.practise.zeto.products.ProductEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import static jakarta.persistence.FetchType.LAZY;
+@Entity
+@Table(name="buyers")
+@Setter
+@Getter
+@NoArgsConstructor
+public class BuyerEntity extends GlobalEntity {
+
+    private String firstName;
+    private  String lastName;
+    private  String email;
+
+    private String password;
+
+    @OneToMany(mappedBy = "buyerEntity", fetch = LAZY)
+        private List<ProductEntity> products;
+}
